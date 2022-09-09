c585ed37ffd9a360ad3eb1acc3242bd972293b3e
Other Orb / Moses Milazzo <moses@otherorb.net>
2022-09-09 04:44:22 -0700

    feat(docker commands for the postgres database): Added a file that describes the simple commands to be used for acquiring the postgres docker image, for shutting down the docker container once it's running, for deleting it from the "to-run" docker list, and for deleting the docker image entirely. Also included example commands for removing the persistent storage of the database.

---
+++ src/visdb/database/docker_commands.sh
@@ -0,0 +1,41 @@
+# I went down a terrible rabbit-hole trying to recreate some docker stuff 
+# using python and then decided that was a waste of time and these
+# commands would do the job just fine. Note that this file is purposefully
+# not executable and does not have bash magic notation at the beginning of the file. 
+# This is so I avoid dangerous, unthinking deletion of docker containers and images 
+# that I don't want to lose. 
+
+# I strongly recommend the commands within parentheses (not including the parentheses) 
+# be run by themselves before attempting to run the destructive commands. The
+# commands here assume there's only one postgresql service running.
+# If that's not true, then one needs to be selective of which one is killed or 
+# deleted.
+
+# If you will be copy-pasting these commands, note that the "-" at the front of the
+# command is meant to make this file not something that can be run with a ./ command
+# Don't copy the "-".
+
+
+# Create a new postgres docker container using the image available in Docker's 
+# service. This relies on the existing docker-compose file in this directory. 
+# 
+- docker-compose pull
+
+# Find the docker container that is a postgresql service
+- docker ps -a | grep -i postgres
+
+# Stop the docker container that is a postgresql service:
+- docker stop $(docker ps -a | grep -i postgres | awk '{print $1}')
+
+# Remove the docker container that is a postgresql service:
+- docker rm $(docker ps -a | grep -i postgres | awk '{print $1}')
+
+# Delete the docker image that is a postgresql service:
+- docker rmi --force $(docker images | grep -i postgres | awk '{print $3}')
+
+# Need superuser privileges to do the following:
+# Delete the db-data directory
+- sudo rm -rf db-data
+
+# Delete the postgres-data directory
+- sudo rm -rf postgres-data
